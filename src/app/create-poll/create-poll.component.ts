76d6774e7a713e0746cfaec7ceeace0371255dde
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2020-05-19 12:37:03 +0530

    new poll ui controls

--- src/app/create-poll/create-poll.component.ts
+++ src/app/create-poll/create-poll.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePollComponent implements OnInit {
 
+  public OptionsList: string[];
   constructor() { }
 
   ngOnInit() {
+    this.OptionsList = ["hello", "test"];
+  }
+
+  addNewOption() {
+    var elements = this.OptionsList.length;
+    this.OptionsList[elements] = "";
+  }
+
+  removeItem(removeItem: number) {
+    this.OptionsList.splice(removeItem, 1);
   }
 
 }
