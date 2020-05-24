d1cd8c54ddb9ae12f3dbeedbadaa95e5cdfdd4bf
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2020-05-24 16:43:59 +0530

    Added dynamic menu style

--- src/app/services/navigation-menu/navigation-menu.service.ts
+++ src/app/services/navigation-menu/navigation-menu.service.ts
@@ -8,11 +8,17 @@ export class NavigationMenuService {
 private navigationMenuItems: NavigationModel[]
 constructor() {
   this.navigationMenuItems = [
-    {name: 'Home', url: '.'},
-    // {name: 'Dashboard', url: 'dashboard'},
-    {name: 'New Poll', url: 'poll/new'},
-    // {name: 'New Survey', url: 'survey/new'},
-    {name: 'Status', url: 'status'}];
+
+    {name: 'Home', url: '.', children:[]},
+    {name: 'Poll', url: '#', children :[
+      {name: 'New Poll', url: 'poll/new', children: []},
+    ]},
+    {name: 'Status', url: 'status', children: []}];
+    // {name: 'Home', url: '.'},
+    // // {name: 'Dashboard', url: 'dashboard'},
+    // {name: 'New Poll', url: 'poll/new'},
+    // // {name: 'New Survey', url: 'survey/new'},
+    // {name: 'Status', url: 'status'}];
 }
 
 get() : NavigationModel[] {
